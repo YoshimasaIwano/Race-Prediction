26bed7bf3fffa49f8f4b032387e920257f1941c0
YoshimasaIwano <44yoshimasa03@gmail.com>
2022-06-01 01:22:10 -0700

    change label smoothing and add callback

--- training/utils.py
+++ training/utils.py
@@ -29,11 +29,36 @@ def create_time_series_data(raw_data):
             horse_number += 1
     return time_series_data, label
 
+def create_time_series_data_weekly(raw_data):
+    number_of_race = raw_data.race_id.nunique()
+    time_series_data = np.full((number_of_race, 24, raw_data.shape[1]-2), 0.0)#-float('inf')
+    race_number = 0
+    horse_number = 0
+    for i in range(len(raw_data)):
+        if i == 0:
+            time_series_data[race_number][horse_number] = raw_data.iloc[i].drop(['race_id','order'])
+            horse_number += 1
+            continue
+        if raw_data.iloc[i].race_id != raw_data.iloc[i-1].race_id:
+            race_number += 1
+            horse_number = 0
+            time_series_data[race_number][horse_number] = raw_data.iloc[i].drop(['race_id','order'])
+            horse_number += 1
+        else:
+            time_series_data[race_number][horse_number] = raw_data.iloc[i].drop(['race_id','order'])
+            horse_number += 1
+    return time_series_data
+
 def smooth_label(label, factor=0.03):
     # smooth label
     label *= (1 - factor)
-    label[:,:,1:4] += (factor / 3)
+#     label[:,:,1:4] += (factor / 3)
 
+    for i in range(label.shape[0]):
+        for j in range(label.shape[1]):
+            t = np.where(label[i][j] == 1 - factor)
+            if t[0][0] != 25:
+                label[i,j,max(0,t[0][0]-1):min(26,t[0][0]+2)] += (factor / 3)
     return label
 
 def categorical_focal_loss(alpha, gamma):
